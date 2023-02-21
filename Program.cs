85c8b395e8cf7cf002df728f19fcbae5fc539668
dsniro <derek.niro@gmail.com>
2023-02-21 14:33:55 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+new DriverManager().SetUpDriver(new ChromeConfig());
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+driver.Navigate().GoToUrl("https://github.com");
+
+IWebElement searchInput = driver.FindElement(By.CssSelector("[name='q']"));
+searchInput.SendKeys("selenium");
+searchInput.SendKeys(Keys.Enter);
+
+driver.Quit();
